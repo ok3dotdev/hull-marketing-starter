98d6bc1ff9479e21514bed650cd0020137eaaefc
Henry Oke <hoke@LMG-00053.local>
2022-04-17 12:28:36 -0400

    Feat: Google Analytics

--- components/layout.js
+++ components/layout.js
@@ -49,18 +49,27 @@ const Layout = ({ site = {}, page = {}, schema, children }) => {
   return (
     <>
       <HeadSEO site={site} page={page} schema={schema} />
-
       {site.gtmID && (
-        <Script
-          id="gtm"
+          <>
+            <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${site.gtmID}`}
+          />
+           <Script
+          id="gtag-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${site.gtmID}');`,
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${site.gtmID}', {
+                page_path: window.location.pathname,
+              });
+            `,
           }}
         />
+        </>
       )}
 
       <m.div
